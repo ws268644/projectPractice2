104419b4180f4820236ff307ce37622929d99e19
Lewis Ingram <WS268644@weston.ac.uk>
2021-10-11 16:14:39 +0100

    MySQL execute scalar

--- Login.cs
+++ Login.cs
@@ -21,21 +21,24 @@ namespace projectPractice2
         private void btnLogin_Click(object sender, EventArgs e)
         {
 
-            String conString = "server=ws265660ProjectPractice.remote.ac;user = WS265660_Admin;database = WS265660_ProjectPractice;password =?Ja6g8j9;CharSet=utf8;SslMode=none;";
+            string conString = "server=ws265660ProjectPractice.remote.ac;user = WS265660_Admin;database = WS265660_ProjectPractice;password =?Ja6g8j9;CharSet=utf8;SslMode=none;";
             MySqlConnection cnn = new MySqlConnection(conString);
+            string command = "SELECT `fname` FROM `t_users` WHERE `ID` = 1";
+            string loginCommand = "SELECT * FROM `t_users` WHERE `password` = " + txtPassword.Text;
+            MySqlCommand myCom = new MySqlCommand(command, cnn);
             try
             {
                 cnn.Open();
                 Console.WriteLine("Database Connected!");
-
+                MessageBox.Show(myCom.ExecuteScalar().ToString());
                 cnn.Close();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("An errot has occured \n" + ex);
             }
-            String username = txtUsername.Text;
-            String password = txtPassword.Text;
+/*            string username = txtUsername.Text;
+            string password = txtPassword.Text;
 
             if (username == "Gamer")
             {
@@ -45,7 +48,7 @@ namespace projectPractice2
                 }
                 else { MessageBox.Show("Password is incorrect."); }
             }
-            else { MessageBox.Show("Login details are incorrect."); }
+            else { MessageBox.Show("Login details are incorrect."); }*/
         }
     }
 }
